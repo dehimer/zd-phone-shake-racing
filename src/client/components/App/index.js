4b1f233be554b4245de2c78d3c3ed28f6f522dfd
Denis Novikov <den-vse@yandex.ru>
2019-02-23 22:04:48 +0300

    refactor: remove long commented code

--- src/client/components/App/index.js
+++ src/client/components/App/index.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import PropTypes from 'prop-types';
 
 
 import SelectPerson from '../SelectPerson';
@@ -40,12 +39,6 @@ class App extends Component {
   }
 }
 
-// App.propTypes = {
-//   // eslint-disable-next-line react/forbid-prop-types
-//   persons: PropTypes.array,
-//   person: PropTypes.object,
-// };
-
 const mapStateToProps = (state) => {
   const { persons, person } = state.server;
   return { persons, person };
@@ -66,66 +59,3 @@ export default connect(
   mapStateToProps,
   mapDispatchToProps
 )(App);
-/*
-class App extends Component {
-  static getDerivedStateFromProps(props, state) {
-    console.log('getDerivedStateFromProps');
-    console.log(props);
-    console.log(state);
-    return null;
-  }
-
-  state = {
-    person: null
-  };
-
-  componentDidMount() {
-    // console.log('componentDidMount');
-    // const userId = localStorage.getItem('userId');
-    // console.log(`userId: ${userId}`);
-  }
-
-  setPerson = ({ id: personId }) => {
-    const { selectPerson } = this.props;
-    console.log('setPerson');
-    // generate player id
-    const userId = uuidv1();
-    this.setState({ userId: uuidv1() }, () => {
-    });
-    // send to server selected person and player id
-    selectPerson({ userId, personId });
-    //
-
-    // this.setState({ person }, () => {
-    //   // send
-    // })
-  };
-
-  derivi
-
-  // const showUserFieldsScreen = false;
-  // зачем хранить в памяти?
-  // нужно в памяти хранить только id клиента и при его соединение и любых операциях использовать его
-  // этот id появляется по завершению ввода данных о себе
-  // check all fields
-
-  // id берётся из localStorage
-
-  // клиент проверяет (где?) есть ли у него в ls id и если нет, то шлёт запрос
-
-  render() {
-    const { persons, person } = this.state;
-
-    return (
-      <div>
-        { persons && !person
-          ? <Persons persons={[]} select={this.setPerson}/>
-          : null
-        }
-      </div>
-    )
-  }
-}
-
-
-*/
