550e80f75709d853316f940d3c7842885c31a141
Denis Novikov <den-vse@yandex.ru>
2019-02-27 21:02:53 +0300

    chore: add frame to selected instrument

--- src/client/components/SelectPerson/index.js
+++ src/client/components/SelectPerson/index.js
@@ -1,27 +1,58 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import styles from './index.css';
 
-// import ScreenName from '../ScreenName';
+export default class SelectPerson extends Component {
+  state = {
+    selectedId: null
+  };
 
-export default function SelectPerson({ persons, select }) {
-  return (
-    <div className={styles.selectPerson}>
-      <div className={styles.title}>Выберите инструмент</div>
-      <div className={styles.persons}>
-        {
-          persons && persons.map(({ id, avatar, name }) => (
-            <div
-              key={id}
-              className={styles.person}
-              onClick={() => select(id)}
-            >
-              <img src={`/public/theme/avatars/${avatar}`}/>
-              <div className={styles.name}>{name}</div>
-            </div>
-          ))
-        }
+  preSelectPerson(id) {
+    const { selectedId } = this.state;
+    if (!selectedId) {
+      console.log(`preSelectPerson ${id}`);
+      this.setState({ selectedId: id });
+
+      setTimeout(() => {
+        this.setState({ selectedId: null });
+        const { select } = this.props;
+        select(id);
+      }, 350);
+    }
+  };
+
+  render() {
+    const { selectedId } = this.state;
+    const { persons } = this.props;
+
+    return (
+      <div className={styles.selectPerson}>
+        <div className={styles.title}>Выберите инструмент</div>
+        <div className={styles.persons}>
+          {
+            persons && persons.map(({ id, avatar, name }) => (
+              <div
+                key={id}
+                className={selectedId === id ? styles.personSelected : styles.person}
+                onClick={() => this.preSelectPerson(id)}
+              >
+                <img src={`/public/theme/avatars/${avatar}`} />
+                <div className={styles.name}>{name}</div>
+              </div>
+            ))
+          }
+        </div>
+        <div className={styles.bottom}>
+          <div className={styles.first}>
+            <img className={styles.left} src={`/public/theme/F-5.png`} />
+            <img className={styles.right} src={`/public/theme/F-1.png`} />
+          </div>
+          <div className={styles.second}>
+            <img className={styles.left} src={`/public/theme/F-4.png`} />
+            <img className={styles.right} src={`/public/theme/F-4.png`} />
+          </div>
+        </div>
       </div>
-    </div>
-  )
+    )
+  }
 }
